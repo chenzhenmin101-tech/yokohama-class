550851a972195a79d3e0ba518c78c436d68ce163
chenzhenmin-101 <chenzhenmin101@gamil.com>
2026-05-28 14:41:56 +0900

    if文の練習

---
+++ 0528/algo1.py
@@ -0,0 +1,24 @@
+x = 10
+s = "python"
+print("xの値は", x)
+print("sの値は", s)
+
+print("=" * 20 ) #　区切り (くぎり)
+# p.40 variable_2.py
+n = 10
+print("nの初期値は", n)
+n = n + 20
+print("nに20を足すと", n, "になります")
+n = 500
+print("nに新たらな", n,"を代入")
+
+print("=" * 20 ) #　区切り (くぎり)
+
+# p.45 if_1.py
+n = 0
+if n > 0:
+    print("nの値は0より大きい")
+if n < 0:
+    print("nの値は0より小さい")    
+if n == 0:
+    print("nの値は0です")
